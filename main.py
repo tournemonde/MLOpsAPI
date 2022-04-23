3d906ecf1a7a4bcfdc012b6f8d91d5a0ce084a00
Jean Paul Tournemonde <tournemonde@tournemonde-2.local>
2022-04-23 23:22:20 +0200

    health logger

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
+import logging
+from logging.config import dictConfig
+from log_config import log_config
+
+dictConfig(log_config)
+logger = logging.getLogger("my-project-logger")
 
 app = FastAPI()
 
 @app.get("/health")
 def health():
+  logger.info("Health request performed")
   return 'Up and running'
 
